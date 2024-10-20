8d7570a5559e8dcdb40cd6afe9815f1117788aec
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-10-20 20:19:32 +0700

    Add files via upload

--- 03-Reviews/app.js
+++ 03-Reviews/app.js
@@ -44,13 +44,13 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener("DOMContentLoaded", function () {
-  showPerson(currentItem);
+  showPerson();
 });
 
 // show person based on item
 
-function showPerson(person){
-  const item = reviews[person];
+function showPerson(){
+  const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
@@ -61,5 +61,24 @@ function showPerson(person){
 
 nextBtn.addEventListener("click", function () {
   currentItem++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+  }
   showPerson(currentItem);
 });
+// show prev person
+
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+// show random person
+randomBtn.addEventListener('click', function(){
+  currentItem = Math.floor(Math.random() * reviews.length);
+  console.logshowPerson(): void
+  showPerson;
+})
