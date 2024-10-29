3fa9950b3d3d40cca715ad935e9c68fca45cfdfa
Korrawit450 <58800719+Korrawit450@users.noreply.github.com>
2024-10-29 21:41:56 +0700

    Add files via upload

--- 04-Navbar/app.js
+++ 04-Navbar/app.js
@@ -7,7 +7,15 @@
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".nav-toggle");
 
-navToggle.addEventListener('click', function(){
-    console.log();
-
-})
+navToggle.addEventListener("click", function(){
+    // console.log(links.classList);
+    // console.log(links.classList.contains("random"));
+    // console.log(links.classList.contains("links"));
+    // if(links.classList.contains("show-links")){
+    //    links.classList.remove("show-links")
+    // }
+    // else{
+    //    links.classList.add("show-links");
+    // }
+    links.classList.toggle("show-links");
+});
